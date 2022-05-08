fix masking

test.py
@@ -1,39 +1,68 @@
 import torch
-from models.argmaxflowv2.model import ContextNet
-from models.argmaxflowv2.conditional import ConditionalAdjacencyBlockFlow
-from models.atomflow import create_mask
+from models import atomflow
 
-from models.dequantization import Dequantization
 
-from utils import get_datasets
 from models.atomflow import AtomFlow
+from models.argmaxflowv2 import ArgmaxFlow
 
+from rdkit import Chem
+
+
+device = torch.device("cpu")
+bond_dict = {1: Chem.rdchem.BondType.SINGLE, 2: Chem.rdchem.BondType.DOUBLE, 3: Chem.rdchem.BondType.TRIPLE, 4: Chem.rdchem.BondType.AROMATIC}
+atom_decoder = ['NO','H', 'C', 'N', 'O', 'F']
+atom_encoder = {'H': 1, 'C': 2, 'N': 3, 'O': 4, 'F': 5}
 
 if __name__ == '__main__':
     
     # transform = T.Compose([ToDenseAdjV2(num_nodes=9)])
     # dataset = ModifiedQM9(root="./mqm9-datasets", pre_transform=transform)
 
+    atomflow_states = torch.load("atom.pt", map_location=device)
+    atomflow = AtomFlow(block_length=6, mask_ratio=9.)
+    atomflow.load_state_dict(atomflow_states['model_state_dict'])
 
-    # for data in dataset:
-    #     if data.adj.shape[0] != 45:
-    #         print(data)
-    #     if data.x.shape[0] != 9:
-    #         print(data)
+    adjflow_states = torch.load("adj.pt", map_location=device)
+    adjflow = ArgmaxFlow(t=6, mask_ratio=9., max_nodes=9, hidden_dim=64)
+    adjflow.load_state_dict(adjflow_states['model_state_dict'])
 
-    t = torch.tensor([[8, 6, 6, 6, 6, 7, 6, 8, 6]])
+    base = torch.distributions.Normal(loc=0., scale=1.)
 
-    deq = Dequantization(alpha=0.01, quants=9)
+    z = base.sample(sample_shape=(1, 1, 45, 5))
+    adj, _ = adjflow.inverse(z)
+    adj = adj.squeeze(1)
 
-    print(deq(t), t.shape)
-    # train_loader, test_loader = get_datasets(type="mqm9", batch_size=128)
+    print(adj.shape, "\n\n", adj[0])
 
-    # batch = next(iter(train_loader))
+    at_z = base.sample(sample_shape=(1, 9, 7))
+    at, _ = atomflow.inverse(at_z, adj)
 
-    # g = AtomFlow(block_length=2, max_nodes=9, context_size=16, hidden_dim=64, inverted_mask=False)
-    
-    # z, ldj = g(batch.x, batch.adj)
-    # x, _ = g.inverse(z, batch.adj)
+    print(at.shape, "\n\n", at[0])
 
-    # print(z.shape, batch.x[0])
-   
+    atom_map = at[0]
+
+
+    adj_t = torch.zeros(9, 9, dtype=torch.int64, device=device)
+    indices = torch.triu_indices(row=9, col=9, offset=0)
+    adj_t[indices[0], indices[1]] = adj[0]
+
+    mol = Chem.RWMol()
+
+    for atom_index in range(atom_map.shape[0]):
+        if atom_map[atom_index, 0] == 0:
+            continue
+
+        mol.AddAtom(Chem.Atom(atom_decoder[atom_map[atom_index, 0]]))
+
+    for i in range(atom_map.shape[0]):
+        for j in range(i + 1, atom_map.shape[0]):
+            if adj_t[i, j] == 0:
+                continue
+
+            bond_rdkit = bond_dict[int(adj_t[i, j])]
+            mol.AddBond(i, j, bond_rdkit)
+            print(i, j, bond_rdkit)
+
+    mol = Chem.SanitizeMol(mol)
+    smiles = Chem.MolToSmiles(mol)
+    print(smiles)

utils/datasets.py
@@ -38,7 +38,7 @@ class ToDenseAdjV2(BaseTransform):
         tmp = torch.ones(data.orig_adj.shape[0], data.orig_adj.shape[1], 1) * 0.5
 
         data.adj = torch.cat((tmp, data.orig_adj), dim=-1).argmax(dim=-1)
-        data.adj = data.adj[torch.tril_indices(9,9).unbind()].long()
+        data.adj = data.adj[torch.triu_indices(9,9).unbind()].long()
 
 
         data.edge_index = None

utils/utils.py
@@ -46,7 +46,7 @@ def create_model_and_optimiser_sche(config):
         states = torch.load(config["load_from"], map_location=device)
 
         model.load_state_dict(states["model_state_dict"])
-        optimiser.load_state_dict(states["optimiser_state_dict"])
+        optimiser.load_state_dict(states["optimizer_state_dict"])
 
         if "scheduler" in config:
             scheduler.load_state_dict(states["scheduler_state_dict"])